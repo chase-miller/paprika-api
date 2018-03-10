93799c4831e4ac04370b2c22eba9df8948f48eb8
Chase Miller <chase.a.miller@gmail.com>
2018-03-10 15:22:16 -0500

    eslint update

--- routes/groceries.js
+++ routes/groceries.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const rp = require('request-promise-native');
 
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
 
     const response = await rp.get('https://www.paprikaapp.com/api/v1/sync/groceries/', {
         'auth': {
             'user': req.query.user,
             'pass': req.query.password,
             'sendImmediately': false
-          }
-    })
+        }
+    });
 
     res.send(response);
 });
